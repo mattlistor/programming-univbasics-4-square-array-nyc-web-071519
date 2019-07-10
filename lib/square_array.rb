402b8c7199aafc7b6ed6fdd82c05a6dea4f98525
Matthew Listor <mattlistor@gmail.com>
2019-07-10 03:28:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   n = 0
-  while array(n)
-    array(n) = array(n) ** 2
+  while array[n]
+    array[n] = array[n] ** 2
     n += 1
   end
-  return array(n)
+  
+  return array
 end
